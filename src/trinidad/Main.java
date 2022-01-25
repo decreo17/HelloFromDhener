b28fd0bcc371b53220b80ad2a5d84c9b88b7e28c
decreo17 <decreogithub@gmail.com>
2022-01-26 07:16:21 +0800

    added code for stretch challenge

--- src/trinidad/Main.java
+++ src/trinidad/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Dhener");
+        stretchChallenge();
+    }
+
+    public static void stretchChallenge() {
+        System.out.println("I don't know what to say.");
     }
 }
